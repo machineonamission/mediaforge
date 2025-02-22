f44c62ef5f476c9fc85c38aa32c87c805c69cbb6
Melody <machineonamission@gmail.com>
2025-02-21 19:59:28 -0600

    yippie

--- src/processing/ffmpeg/heartlocket.py
+++ src/processing/ffmpeg/heartlocket.py
@@ -6,42 +6,36 @@ from utils.tempfiles import TempFile, reserve_tempfile
 
 async def heart_locket(media: TempFile):
     out = reserve_tempfile("mkv")
-    # media = await resize(media, 256,256)
     w,h = await get_resolution(media)
-    scalefactor = 16
-    pixelscalefactor = 1
-    # xmap = f"({w}*(r(X, Y)*{pixelscalefactor}/{scalefactor} + {scalefactor}*mod(b(X, Y)*{pixelscalefactor},{scalefactor}))  )/255"
-    # ymap = f"({h}*(g(X, Y)*{pixelscalefactor}/{scalefactor} + {scalefactor}*floor(b(X, Y)*{pixelscalefactor}/{scalefactor})))/255"
 
-    # xmap = "X"
-    # ymap = "Y"
+    # you would not FUCKING believe how painful these equations were to derive.
+    # hours of trying random things and chatgpt
     xmap = (
-        f"( ( (floor(r(X,Y)/255)) )"
+        f"(floor(r(X,Y)/255)"
         f" + 256*mod(floor(b(X,Y)/255),16) )"  # 0..255
         f" * {w} / 4096"                  # scale to 0..w
     )
     ymap = (
-        f"( ( (floor(g(X,Y)/255)) )"
+        f"(floor(g(X,Y)/255)"
         f" + 256*floor(floor(b(X,Y)/255)/16) )"  # 0..255
         f" * {h} / 4096"                    # scale to 0..h
     )
-    # xmap = "0.5"
-    # ymap = "0.5"
     await run_command(
         "ffmpeg",
         "-i", media,  # Your main image/video
-        "-i", "rendering/images/heartlocket/38/mapper.png",  # The color "every pixel" map
+        "-i", "rendering/images/heartlocket/mapper/38.png",  # The color "every pixel" map
         "-filter_complex",
         (
+            # pre-process
             "[1:v]format=rgba64,split=2[1v1][1v2];"
-            # 1) Create the X map (16-bit grayscale) from R/B
+            # convert from weird proprietary color thing to x map
             f"[1v1]geq=r='{xmap}':g='{xmap}':b='{xmap}',format=gray16le[xmap];"
-            # 2) Create the Y map (16-bit grayscale) from G/B
+            # convert from weird proprietary color thing to y map
             f"[1v2]geq=r='{ymap}':g='{ymap}':b='{ymap}',format=gray16le[ymap];"
-            # 3) Remap the main input using absolute mode
-            "[0:v][xmap][ymap]remap"
+            # map the input image onto the locket
+            "[0:v][xmap][ymap]remap,format=rgba"#,split=2[mapped1][mapped2];"
         ),
-        "-c:v", "ffv1", "-fps_mode", "vfr",
+        "-c:v", "ffv1", "-fps_mode", "vfr",# "-shortest",
         out)
     # await run_command("ffplay", out)
     return out
