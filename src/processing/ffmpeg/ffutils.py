2a4a5940c047f55a87d85d9376f11cbf24e0d293
Melody <machineonamission@gmail.com>
2025-03-06 01:51:28 -0600

    closes #234 probably

--- src/processing/ffmpeg/ffutils.py
+++ src/processing/ffmpeg/ffutils.py
@@ -224,7 +224,7 @@ async def resize(image, width, height, lock_codec=False):
     """
     gif = await image.mediatype() == GIF
     ext = image.split(".")[-1]
-    out = reserve_tempfile(ext if lock_codec else "mkv")
+    out = reserve_tempfile(ext if lock_codec and not gif else "mkv")
     await run_command("ffmpeg", "-i", image, "-max_muxing_queue_size", "9999", "-sws_flags",
                       "spline+accurate_rnd+full_chroma_int+full_chroma_inp+bitexact",
                       "-vf", f"scale='{width}:{height}',setsar=1:1", "-c:v", "copy" if lock_codec and not gif else "ffv1",
